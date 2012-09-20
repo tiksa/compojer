2969cf70cd7bf5e5cb3362a045baa163a97e52ce
tiksa <timo.l.saarinen@gmail.com>
2012-09-20 21:48:46 +0300

    Added empty clojure project

---
+++ src/compojer/core.clj
@@ -0,0 +1,4 @@
+(ns compojer.core (:gen-class))
+
+(defn -main [& args]
+    (println "Compojer v0.1"))
---
+++ test/compojer/test/core.clj
@@ -0,0 +1,6 @@
+(ns compojer.test.core
+  (:use [compojer.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
