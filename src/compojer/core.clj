bcb2f628c78cf5fd8ec19b80d9ee863a1c775ee1
tiksa <timo.l.saarinen@gmail.com>
2012-09-21 23:34:02 +0300

    Minor fixes

--- src/compojer/core.clj
+++ src/compojer/core.clj
@@ -2,7 +2,7 @@
     
 (def notes [])
 
-(def pitch-symbols ['c1 'd1 'e1 'f1 'g1 'a1 'h1 'c2 'd2 'e2 'f2 'g2 'a2 'h2])
+(def pitch-symbols ['c1 'd1 'e1 'f1 'g1 'a1 'h1 'c2 'd2 'e2])
 (def duration-symbols ['1/8 '1/4 '1/2])
 
 (def available-pitches (range (count pitch-symbols)))
@@ -39,11 +39,11 @@
 	current
 	(if (zero? (count current))
 	    (cumulate-recur
-		(rest distribution)
-		(conj current (first distribution)))
+            (rest distribution)
+            (conj current (first distribution)))
 	    (cumulate-recur
-		(rest distribution)
-		(conj current (+ (first distribution) (last current)))))))
+            (rest distribution)
+            (conj current (+ (first distribution) (last current)))))))
 
 (defn cumulate [distribution]
     (cumulate-recur distribution []))
@@ -61,7 +61,7 @@
         notes 
         (add-notes (conj notes (gen-note
             (if (empty? notes)
-                [8 2]
+                [4 2]
 				(last notes)))) (dec n))))
                 
 (defn nums-to-symbols [notes]
@@ -69,7 +69,9 @@
         notes
         (cons [(nth pitch-symbols (first (first notes))) (nth duration-symbols (second (first notes)))]
             (nums-to-symbols (rest notes)))))
-    
+
 (defn -main [& args]
         (println "Compojer v0.1")
-        (println (nums-to-symbols (add-notes notes 10))))
+        (println "How long piece shall I compose? (notes)")
+        (let [num-of-notes (Integer. (read-line))]
+            (println (nums-to-symbols (add-notes notes num-of-notes)))))
